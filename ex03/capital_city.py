6d46fcf3b655217251fe720506515197ff2fd455
yuki-katayama <katayu810@gmail.com>
2021-02-02 12:54:09 +0900

    ex03_complete

---
+++ ex03/capital_city.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    states = {
+    	"Oregon" : "OR",
+	    "Alabama" : "AL",
+    	"New Jersey": "NJ",
+	    "Colorado" : "CO"
+	}
+    capital_cities = {
+    	"OR": "Salem",
+	    "AL": "Montgomery",
+    	"NJ": "Trenton",
+	    "CO": "Denver"
+    }
+
+    if(len(sys.argv) >= 3):
+        sys.exit()
+    try:
+        output = capital_cities[states[sys.argv[1]]]
+    except KeyError:
+        output = "Unknown state"
+    except:
+        output = "Unknown state"
+    print(output)
+
+if __name__ == "__main__":
+    main()
