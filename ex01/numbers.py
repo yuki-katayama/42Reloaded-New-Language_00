7cab3793d8a8789f6e0892a4426fcdc1bbc3cb99
yuki-katayama <katayu810@gmail.com>
2021-02-02 12:28:16 +0900

    ex01_complete

--- ex01/numbers.py
+++ ex01/numbers.py
@@ -1,5 +1,9 @@
-f = open("numbers.txt", "r")
-data = f.readlines()
-for value in data:
-    value = value.strip(",\n")
-    print(value)
+def main():
+    f = open("numbers.txt", "r")
+    data = f.readlines()
+    for value in data:
+        value = value.strip(",\n")
+        print(value)
+
+if __name__ == "__main__":
+    main()
